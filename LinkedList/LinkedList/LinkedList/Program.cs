c6ee2ec8189d6c5af58828dbf88cb04744d733a8
Abdalqader-Froukh <abdalqaderfroukh1@gmail.com>
2026-08-01 21:59:59 +0300

    Complete LinkedList Tasks

---
+++ LinkedList/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1
+            LinkedList<string> navigation = new LinkedList<string>();
+
+            navigation.AddFirst("Home");
+            navigation.AddLast("About");
+            navigation.AddLast("Contact");
+
+            foreach (string page in navigation)
+            {
+                Console.WriteLine(page);
+            }
+
+            //2
+            navigation.Remove("About");
+
+
+            //3
+            LinkedListNode<string> contactNode = navigation.Find("Contact");
+
+            if (contactNode != null)
+            {
+                navigation.AddBefore(contactNode, "About");
+            }
+
+            foreach (string page in navigation)
+            {
+                Console.WriteLine(page);
+            }
+
+            //4
+            foreach (string page in navigation)
+            {
+                Console.WriteLine(page);
+            }
+
+            //5
+            Console.WriteLine( navigation.First.Value);
+            Console.WriteLine( navigation.Last.Value);
+        }
+    }
+}
